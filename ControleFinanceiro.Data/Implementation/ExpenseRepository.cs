6679f2c08bbd623288ac1e02dd4cd307c8d361db
ruangb <ruangbarros@gmail.com>
2024-06-18 20:07:13 -0300

    Implementação da chamada da Procedure de gravar parcelas e faturas

--- ControleFinanceiro.Data/Implementation/ExpenseRepository.cs
+++ ControleFinanceiro.Data/Implementation/ExpenseRepository.cs
@@ -3,6 +3,8 @@ using ControleFinanceiro.Data.Interfaces;
 using ControleFinanceiro.Models;
 using System.Data.SqlClient;
 using Dapper;
+using System.Data;
+using System.Transactions;
 
 namespace ControleFinanceiro.Data.Implementation
 {
@@ -42,43 +44,92 @@ namespace ControleFinanceiro.Data.Implementation
             return ExecuteGetById(id);
         }
 
+        //public int Insert(Expense entity)
+        //{
+        //    using (var conn = new SqlConnection(_context.GetConnectionString()))
+        //    {
+        //        conn.Open();
+        //        SqlTransaction tran = conn.BeginTransaction();
+
+        //        try
+        //        {
+        //            using (var command = conn.CreateCommand())
+        //            {
+        //                List<string> fieldNames = [];
+        //                DataSupport<Expense>.SetCommandParametersForInsertByEntityValues(entity, command, fieldNames);
+        //                string sqlInsertExpense = DataSupport<Expense>.GenerateSqlInsert(fieldNames);
+
+        //                command.CommandText = sqlInsertExpense;
+        //                command.Transaction = tran;
+        //                SqlDataReader reader = command.ExecuteReader();
+  
+        //                while (reader.Read())
+        //                {
+        //                    entity.Id = reader.GetInt32(0);
+        //                }
+
+        //                _expenseInstallment.InsertByExpense(entity, command);
+
+        //                tran.Commit();
+
+        //                return entity.Id;
+        //            }
+        //        }
+        //        catch (Exception)
+        //        {
+        //            tran.Rollback();
+        //            throw;
+        //        }
+        //    }
+        //}
+
         public int Insert(Expense entity)
         {
-            using (var conn = new SqlConnection(_context.GetConnectionString()))
-            {
-                conn.Open();
-                SqlTransaction tran = conn.BeginTransaction();
+            int id;
 
-                try
+            using (var tranScope = new TransactionScope())
+            {
+                using (var conn = new SqlConnection(_context.GetConnectionString()))
                 {
+                    conn.Open();
+                    //SqlTransaction tran = conn.BeginTransaction();
+
                     using (var command = conn.CreateCommand())
                     {
                         List<string> fieldNames = [];
+
                         DataSupport<Expense>.SetCommandParametersForInsertByEntityValues(entity, command, fieldNames);
-                        string sqlInsertExpense = DataSupport<Expense>.GenerateSqlInsert(fieldNames);
 
-                        command.CommandText = sqlInsertExpense;
-                        command.Transaction = tran;
-                        SqlDataReader reader = command.ExecuteReader();
-  
-                        while (reader.Read())
-                        {
-                            entity.Id = reader.GetInt32(0);
-                        }
+                        command.CommandText = DataSupport<Expense>.GenerateSqlInsert(fieldNames);
 
-                        _expenseInstallment.InsertByExpense(entity, command);
-
-                        tran.Commit();
-
-                        return entity.Id;
+                        id = (int)command.ExecuteScalar();
+                        //tran.Commit();
                     }
-                }
-                catch (Exception)
-                {
-                    tran.Rollback();
-                    throw;
+
+                    decimal parcelValue = entity.Amount / entity.ParcelQuantity;
+
+                    for (int i = 1; i <= entity.ParcelQuantity; i++)
+                    {
+                        var result = conn.ExecuteReader("PRC_SAVE_EXPENSE_INSTALLMENT",
+                            new
+                            {
+                                N_ID = 0,
+                                N_ID_CREDIT_CARD = entity.IdCreditCard,
+                                N_ID_EXPENSE = id,
+                                N_INSTALLMENT = i,
+                                S_STATUS = entity.Status,
+                                D_REFERENCE_DATE = entity.OperationDate.AddMonths(i - 1),
+                                N_VALUE = parcelValue,
+                                RESULT = "",
+                                RESULT_MESS = 0,
+                            }, commandType: CommandType.StoredProcedure);
+                    }
+
+                    tranScope.Complete();
                 }
             }
+
+            return id;
         }
 
         public void Update(Expense entity)
